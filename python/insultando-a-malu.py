a2eb83a98730a07d6b8dc6e62230098e3d31db48
Adrián Fuentes <adrianfuenteshernandez63@gmail.com>
2021-04-05 12:28:40 +0100

    generador de insultos mejorado

--- python/insultando-a-malu.py
+++ python/insultando-a-malu.py
@@ -1,4 +1,4 @@
 import random
-insulto = ["daltonica", "anorexica", "mal parida"]
-sustantivo = ["puta", "foca", "mamarracha"]
+insulto = ["daltonica", "anorexica", "mal parida", "lamecalvas", "autista", "manca"]
+sustantivo = ["puta", "foca", "mamarracha", "morsa", "orca", "araña", "semaforo", "zorra", "elefante"]
 print("Malu", random.choice(sustantivo), random.choice(insulto))
